cf1d629367e12adcbf11cbd4a7d5168893526a67
dingyonggang <123611684@qq.com>
2016-07-13 10:06:40 +0800

    Initial Commit

---
+++ SBVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBVideoPlayer
+//
+//  Created by bigbomac02 on 16/7/13.
+//  Copyright © 2016年 bigbomac02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SBVideoPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SBVideoPlayer
+//
+//  Created by bigbomac02 on 16/7/13.
+//  Copyright © 2016年 bigbomac02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
